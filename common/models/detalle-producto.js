d79488d329aeee5f257f25f103e1234c612658bf
inaban <25hbaam@gmail.com>
2017-09-27 13:14:20 -0500

    stock

--- common/models/detalle-producto.js
+++ common/models/detalle-producto.js
@@ -2,4 +2,25 @@
 
 module.exports = function(Detalleproducto) {
 
+	Detalleproducto.stock = function(cb){
+		Detalleproducto.getDataSource().connector.connect((err, db) => {
+			let detalleProductoCollection = db.collection('DetalleProducto');
+			detalleProductoCollection.aggregate([
+				{
+					$match: { vendido: {$eq: false}}
+				},
+	    	{ $group: {
+		      _id: '$productoId',
+		    	cantidad: { $sum: 1 },
+		    }}
+			], (err, data) => {
+		    cb(null, data);
+			})
+		})
+	}
+
+	Detalleproducto.remoteMethod('stock', {
+    //accepts: {arg: 'filter', type: 'object', description: '{"where:{...}, "groupBy": "field"}'},
+    returns: {arg:'data', type:['object'], root:true}
+  });
 };
